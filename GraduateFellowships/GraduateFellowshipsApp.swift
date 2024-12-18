0f46ce1fdcc958d3acfaeba09ccbd37ef2f7617d
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2024-12-17 19:36:02 -0500

    Initial Commit

---
+++ GraduateFellowships/GraduateFellowshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraduateFellowshipsApp.swift
+//  GraduateFellowships
+//
+//  Created by Carson Mulligan on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GraduateFellowshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
